c496a9f103f3f6155a5ebf0d96fc698896a8ce09
ivzaislu <johann0789@googlemail.com>
2026-09-22 03:44:52 +0200

    Export local catalog and series tools

--- src/litres_parser/__init__.py
+++ src/litres_parser/__init__.py
@@ -1,3 +1,4 @@
+from .catalog import LitResCatalog, authors_from_art, normalize_key, series_claims_from_art
 from .client import (
     DEFAULT_API_BASE_URL,
     DEFAULT_WEB_BASE_URL,
@@ -6,12 +7,21 @@ from .client import (
     LitResClient,
     LitResFacetsPage,
 )
+from .crawler import CrawlResult, LitResCatalogCrawler
+from .series import LitResSeriesResolver
 
 __all__ = [
     "DEFAULT_API_BASE_URL",
     "DEFAULT_WEB_BASE_URL",
     "MAX_FACETS_PAGE_SIZE",
     "MAX_SERIES_PAGE_SIZE",
+    "CrawlResult",
+    "LitResCatalog",
+    "LitResCatalogCrawler",
     "LitResClient",
     "LitResFacetsPage",
+    "LitResSeriesResolver",
+    "authors_from_art",
+    "normalize_key",
+    "series_claims_from_art",
 ]
